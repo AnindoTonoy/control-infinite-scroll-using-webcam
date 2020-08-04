20d7ca9f0c576f6674650495c8951562738a1056
AnindoTonoy <anindo350@gmail.com>
2020-08-04 23:37:35 +0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+Control infinite scroll using webcam
+Author: Anindo Dey
+Email: anindo350@gamil.com
+"""
+
+import cv2
+import numpy as np
+import pyautogui
+
+cap = cv2.VideoCapture(0)
+
+blue_lower = np.array([100, 150, 0])
+blue_upper = np.array([140, 255, 255])
+
+prev_y = 0
+
+while True:
+    ret, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    mask = cv2.inRange(hsv, blue_lower, blue_upper)
+    contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for c in contours:
+        area = cv2.contourArea(c)
+        if area > 500:
+            # cv2.drawContours(frame,c,-1,(0,255,0),2)
+            # print(area)
+            x, y, w, h = cv2.boundingRect(c)
+            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            if y < prev_y:
+                pyautogui.press('space')
+
+            prev_y =y
+
+    # cv2.imshow('mask',mask)
+    cv2.imshow('Frame', frame)
+
+    if cv2.waitKey(10) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
